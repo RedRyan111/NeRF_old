2b6a4b9c3f3be5d6f75f04e9ee3da761e895737d
RedRyan111 <ravenengineerryan@gmail.com>
2023-11-09 23:58:09 -0600

    Add more printing to Data Manager

--- data_manager.py
+++ data_manager.py
@@ -11,12 +11,16 @@ class DataManager:
         self.poses = self.data['poses']
         self.focal = self.data['focal']
 
+        self.dirs = np.stack([np.sum([0, 0, -1] * pose[:3, :3], axis=-1) for pose in self.poses])
+        self.origins = self.poses[:, :3, -1]
+
         self.num_of_images = self.images.shape[0]
         self.image_height = self.images.shape[1]
         self.image_width = self.images.shape[2]
 
         self.print_data_info()
         self.print_example_image_data()
+        self.print_data_camera_poses_and_directions()
 
     def print_data_info(self):
         print(f'Images shape: {self.images.shape}')
@@ -30,3 +34,20 @@ class DataManager:
         plt.imshow(example_img)
         print('Pose')
         print(example_pose)
+        plt.savefig('example_images/example.png')
+        plt.close()
+
+    def print_data_camera_poses_and_directions(self):
+        ax = plt.figure(figsize=(12, 8)).add_subplot(projection='3d')
+        _ = ax.quiver(
+          self.origins[..., 0].flatten(),
+          self.origins[..., 1].flatten(),
+          self.origins[..., 2].flatten(),
+          self.dirs[..., 0].flatten(),
+          self.dirs[..., 1].flatten(),
+          self.dirs[..., 2].flatten(), length=0.5, normalize=True)
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('z')
+        plt.savefig('example_images/camera_poses_and_directions.png')
+        plt.close()
