34c9f2450db654853f72b4180c15c57d00bd8bed
RedRyan111 <ravenengineerryan@gmail.com>
2023-11-10 22:34:50 -0600

    Refactor volume rendering file

--- volume_rendering.py
+++ volume_rendering.py
@@ -2,58 +2,53 @@ import torch
 
 
 def get_rays(height: int, width: int, focal_length: float) -> torch.Tensor:
-    r"""
-    Find origin and direction of rays through every pixel and camera origin.
-    """
+    row_meshgrid, col_meshgrid = get_meshgrid(height, width)
+    directions = get_ray_directions_from_meshgrid(row_meshgrid, col_meshgrid, height, width, focal_length)
+    return directions
 
-    # Apply pinhole camera model to gather directions at each pixel
-    # create grid
-    i, j = torch.meshgrid(
+
+def transform_rays_with_image_orientation(rays, camera_to_world_spatial_transformation):
+
+    camera_to_world_obj = CameraToWorldSpatialTransformationManager(camera_to_world_spatial_transformation)
+
+    rays_d = camera_to_world_obj.orient_batch_of_rays(rays)
+    rays_o = camera_to_world_obj.expand_translation_to_fit_ray_batches(rays)
+
+    return rays_o, rays_d
+
+
+def get_meshgrid(height, width):
+    row_meshgrid, col_meshgrid = torch.meshgrid(
         torch.arange(height, dtype=torch.float32),
         torch.arange(width, dtype=torch.float32),
         indexing='ij')
+    return row_meshgrid, col_meshgrid
 
-    print(f'i shape: {i.shape}')
 
-    # x, y, z where z is 1 since we're defining a plane that's 1 away
+def get_ray_directions_from_meshgrid(row_meshgrid, col_meshgrid, height, width, focal_length):
     directions = torch.stack([
-        (j - width * .5) / focal_length,
-        -(i - height * .5) / focal_length,
-        -torch.ones_like(i)
+        rescale_meshgrid_to_fit_pixels(col_meshgrid, width, focal_length),
+        rescale_meshgrid_to_fit_pixels(row_meshgrid, height, focal_length),
+        -torch.ones_like(row_meshgrid)
     ], dim=-1)
 
     return directions
 
 
-def transform_rays_with_image_orientation(rays, c2w):
-    print(f'directions: {rays.shape}')
-    print(f'spliced directions: {rays[..., None, :].shape}')
-    print(f'c2w: {c2w.shape} to {c2w[:3, :3].shape}')
-
-    # Apply camera pose transformation to newly created pixel directions
-    rays_d = torch.matmul(rays, c2w[:3, :3].T)
-
-    # Origin is same for all directions (the optical center)
-    rays_o = c2w[:3, -1].expand(rays_d.shape)  # translation part of spatial matrix
-    return rays_o, rays_d
+def rescale_meshgrid_to_fit_pixels(meshgrid, length, focal_length):
+    return (meshgrid - (.5 * length)) / focal_length
 
 
-def get_meshgrid(height, width):
-    i, j = torch.meshgrid(
-        torch.arange(width, dtype=torch.float32),
-        torch.arange(height, dtype=torch.float32),
-        indexing='ij')
-    return i, j
-
-
-class CameraToWorldSpatialTransformation:
+class CameraToWorldSpatialTransformationManager:
     def __init__(self, spatial_matrix):
         self.spatial_matrix = spatial_matrix
         self.orientation = spatial_matrix[:3, :3]
         self.translation = spatial_matrix[:3, -1]
 
+    # Apply camera pose transformation to newly created pixel directions
     def orient_batch_of_rays(self, rays):
         return torch.matmul(rays, self.orientation.T)
 
+    # Origin is same for all directions (the optical center)
     def expand_translation_to_fit_ray_batches(self, rays):
         return self.translation.expand(rays.shape)
